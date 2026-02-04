681570dd5ce4806b4058efc50691fdeadb717963
Valentin Chiriac <Valentin.Chiriac@endava.com>
2026-02-04 14:46:55 +0200

    first lessons of JS

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,57 @@
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript is Fun!");
+
+console.log(40 + 23 + 129 + 2000);
+
+let firstName = "Valentin";
+console.log(firstName);
+
+let country = "Romania";
+let continent = "Europe";
+let population = 19000000;
+
+console.log(
+  "I live in " +
+    country +
+    ", located in " +
+    continent +
+    ", having a population of " +
+    population +
+    " people.",
+);
+let iWantAProject = true;
+
+console.log(typeof firstName);
+console.log(typeof population);
+console.log(typeof iWantAProject);
+
+console.log(iWantAProject);
+
+let project;
+console.log(typeof project);
+
+console.log(typeof null);
+
+iWantAProject = "Yes!";
+console.log(typeof iWantAProject);
+
+let myAge = 34;
+myAge = 36;
+
+const birthYear = 1980;
+console.log(birthYear);
+// birthYear = 1982;
+
+const now = 2026;
+const ageValentin = now - 1980;
+const ageZada = now - 2005;
+console.log(ageValentin, ageZada);
+
+console.log(ageValentin / 2, ageZada * 2);
+
+console.log(ageValentin >= ageValentin);
+console.log(ageValentin >= ageZada);
+console.log(ageZada >= 18);
+
+const isFullAge = ageZada >= 18;
+console.log("Zada is major: " + isFullAge);
